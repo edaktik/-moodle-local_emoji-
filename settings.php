fafab2194a524a7d7dd4029b20a4134a74d4d454
Luca Bösch <luca.boesch@bfh.ch>
2018-06-19 17:45:49 +0200

    Comply to coding standards.

--- settings.php
+++ settings.php
@@ -30,48 +30,40 @@
 
 defined('MOODLE_INTERNAL') || die();
 require_once('../config.php');
-// Ensure the configurations for this site are set
+// Ensure the configurations for this site are set.
 if ( $hassiteconfig ){
-    
+
     $images = array("angry", "approve", "biggrin", "blackeye", "blush", "clown", "cool", "dead", "egg", "evil", "heart", "kiss", "martin", "mixed", "no", "sad", "shy", "sleepy", "smiley", "surprise", "thoughtful", "tongueout", "wideeyes", "wink", "yes");
-    
-    
-    // Create the new settings page
+
+    // Create the new settings page.
     $settings = new admin_settingpage( 'local_emoji', 'Emoji' );
-    
+
     $standardlink = '<a href="'.new moodle_url('/local/emoji/convert.php?id=s').'">'.new lang_string('usestandard', 'local_emoji').'</a>';
-    
-                   
+
     $standardlink .= '<br />';
-    
+
     foreach($images as $value){
         $standardlink .= '<img width="15" height="15" src="'.new moodle_url('/local/emoji/pix/s/'.$value.'.svg').'"> ';
     }
-            
-            
+
     $settings->add(new admin_setting_heading('usestandard', new lang_string('usestandard', 'local_emoji'), $standardlink));
-    
-    
+
     $fancylink = '<a href="'.new moodle_url('/local/emoji/convert.php?id=f').'">'.new lang_string('usefancy', 'local_emoji').'</a>';
-    
-    
+
     $fancylink.= '<br />';
-    
+
     foreach($images as $value){
         $fancylink.= '<img width="15" height="15" src="'.new moodle_url('/local/emoji/pix/f/'.$value.'.svg').'"> ';
     }
-    
-    
-    
+
     $settings->add(new admin_setting_heading('usefancy', new lang_string('usefancy', 'local_emoji'), $fancylink));
-    
+
     $resetlink = '<a href="resetemoticons.php">'.new lang_string('emoticonsreset', 'admin').'</a>';
-    
+
     $settings->add(new admin_setting_heading('configintro', new lang_string('emoticonsreset', 'admin'), $resetlink));
-    
 
     $ADMIN->add('local_emoji', $settings);
-    
-    // Create
+
+    // Create.
     $ADMIN->add( 'localplugins', $settings );
 }
